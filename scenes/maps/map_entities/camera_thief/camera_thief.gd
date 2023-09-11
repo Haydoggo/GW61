17162e78bfee19553d1f52a31120bb3967c32b0f
Hayden Leete <haydenleete@gmail.com>
2023-09-12 10:26:35 +1200

    Camera thief tweaks

--- scenes/maps/map_entities/camera_thief/camera_thief.gd
+++ scenes/maps/map_entities/camera_thief/camera_thief.gd
@@ -14,16 +14,14 @@ func _ready() -> void:
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	var camera = get_viewport().get_camera_2d()
-	camera.set_process(false)
 	var rect = marker.get_global_rect()
-	var c_pos = rect.position + rect.size/2
+	camera.external_target_position = rect.position + rect.size/2
 	var zoom = Vector2.ONE * (get_window().size.y / rect.size.y)
 	var t = create_tween()
 	t.set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_CUBIC)
-	t.tween_property(camera, "position", c_pos, travel_time)
+	t.tween_property(camera, "target_selection", 0.0, travel_time)
 	t.parallel().tween_property(camera, "zoom", zoom, travel_time)
 	if oneshot:
 		t.tween_interval(linger_time)
-		t.tween_property(camera, "position", Player.instance.global_position, travel_time)
+		t.tween_property(camera, "target_selection", 1.0, travel_time)
 		t.parallel().tween_property(camera, "zoom", Vector2(0.5,0.5), travel_time)
-		t.tween_callback(camera.set_process.bind(true))
