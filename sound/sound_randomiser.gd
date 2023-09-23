1772ebf83b36f4eef00fff8a4ef6ad2287f47ffb
Hayden Leete <haydenleete@gmail.com>
2023-09-23 22:32:05 +1200

    Fix lingering shatter sfx nodes

--- sound/sound_randomiser.gd
+++ sound/sound_randomiser.gd
@@ -1,10 +1,14 @@
 extends AudioStreamPlayer
 @export var sounds : Array[AudioStream]
 
+
 func play_random():
 	var random_stream = sounds.pick_random() as AudioStream
 	var asp = duplicate(0)
 	asp.stream = random_stream
 	add_child(asp)
 	asp.play()
-	asp.finished.connect(asp.queue_free)
+	asp.finished.connect(func():
+		asp.queue_free
+		finished.emit()
+		)
